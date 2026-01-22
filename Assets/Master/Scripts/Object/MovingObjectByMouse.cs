59273c6e276ec44052f6442dcb5da3a6b2c8741b
thuannm <thuannm.ot@gmail.com>
2026-01-22 13:57:51 +0700

    - Update : set up the test tubes + material of them

--- Assets/Master/Scripts/Object/MovingObjectByMouse.cs
+++ Assets/Master/Scripts/Object/MovingObjectByMouse.cs
@@ -6,7 +6,6 @@ public class MovingObjectByMouse : MonoBehaviour
 {
     [SerializeField] private Renderer m_BoundaryCube;
     [SerializeField] private bool m_UseSpecialYLimit;
-    [SerializeField] private DragCategory m_DragCategory = DragCategory.None;
     private Vector3 m_Offset;
     public bool m_IsDragging = false;
     private Bounds m_Bounds;
@@ -28,8 +27,6 @@ public class MovingObjectByMouse : MonoBehaviour
     private void OnMouseDown()
     {
         if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject()) return;
-        if (MouseDragLock.IsBlocked && m_DragCategory != DragCategory.None)return;
-
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z; 
         m_Offset = transform.position - Camera.main.ScreenToWorldPoint(mousePosition);
@@ -38,7 +35,6 @@ public class MovingObjectByMouse : MonoBehaviour
     private void OnMouseDrag()
     {
         if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject()) return;
-        if (MouseDragLock.IsBlocked && m_DragCategory != DragCategory.None) return;
         if (m_IsDragging)
         {
             Vector3 mousePosition = Input.mousePosition;
